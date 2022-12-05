a9ac0a95da844fcbbd98aef3646c3e7422951162
Resi Wicaksono <resiwicaksono98@gmail.com>
2022-12-06 01:21:07 +0700

    check user login all components

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,49 +1,81 @@
-import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import DaftarPenyakit from './pages/DaftarPenyakit';
-import Homepage from './pages/Homepage';
-import HomepageAdmin from './pages/admin/Homepage';
-import Penyakit from './pages/Penyakit';
-import DaftarGejala from './pages/DaftarGejala';
-import HasilDiagnosa from './pages/HasilDiagnosa';
-import RiwayatKonsultasi from './pages/RiwayatKonsultasi';
-import Auth from './pages/admin/Auth';
-import PertanyaanPertama from './pages/PertanyaanPertama';
-import Pertanyaan from './pages/Pertanyaan';
-import TambahPenyakit from './pages/admin/TambahPenyakit';
-import TambahGejala from './pages/admin/TambahGejala';
-import Manage from './pages/admin/Manage';
-import EditGejala from './pages/admin/EditGejala';
-import EditPenyakit from './pages/admin/EditPenyakit';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import DaftarPenyakit from "./pages/DaftarPenyakit";
+import Homepage from "./pages/Homepage";
+import HomepageAdmin from "./pages/admin/Homepage";
+import Penyakit from "./pages/Penyakit";
+import DaftarGejala from "./pages/DaftarGejala";
+import HasilDiagnosa from "./pages/HasilDiagnosa";
+import RiwayatKonsultasi from "./pages/RiwayatKonsultasi";
+import Auth from "./pages/admin/Auth";
+import PertanyaanPertama from "./pages/PertanyaanPertama";
+import Pertanyaan from "./pages/Pertanyaan";
+import TambahPenyakit from "./pages/admin/TambahPenyakit";
+import TambahGejala from "./pages/admin/TambahGejala";
+import Manage from "./pages/admin/Manage";
+import EditGejala from "./pages/admin/EditGejala";
+import EditPenyakit from "./pages/admin/EditPenyakit";
+import { useDispatch, useSelector } from "react-redux";
+import { me } from "./redux/userReducers";
 
 const MainRouter = () => {
-	const user = useSelector((state) => state.user.currentUser);
-	
-	return (
-		<Routes>
-			<Route exact path="/" element={<Homepage />} />
-			<Route path="/penyakit" element={<DaftarPenyakit />} />
-			<Route path="/penyakit/:id" element={<Penyakit />} />
-			<Route path="/gejala" element={<DaftarGejala />} />
-			<Route path="/gejala/gejala" element={<Penyakit />} />
-			<Route path="/konsultasi" element={<PertanyaanPertama />} />
-			<Route path="/q/:id" element={<Pertanyaan />} />
-			<Route path="/q2/:id" element={<Pertanyaan />} />
-			<Route path="/hasil/:id" element={<HasilDiagnosa />} />
-			<Route path="/hasil" element={<HasilDiagnosa data={null} />} />
-			<Route path="/riwayat" element={<RiwayatKonsultasi />} />
+  const dispatch = useDispatch();
+  const { currentUser, isLoading, isSuccess } = useSelector(
+    (state) => state.user
+  );
+  useEffect(() => {
+    dispatch(me());
+  }, [dispatch]);
 
-			{/* Admin Pages */}
-			<Route path="/masuk" element={user ? <Navigate to="/" /> : <Auth />} />
-			<Route path="/admin" element={user ? <HomepageAdmin /> : <Navigate to={"/masuk"}/>} />
-			<Route path="/admin/tambah/penyakit" element={user ? <TambahPenyakit />: <Navigate to={"/masuk"}/>} />
-			<Route path="/admin/edit/penyakit/:id" element={user ? <EditPenyakit /> : <Navigate to={"/masuk"}/>} />
-			<Route path="/admin/tambah/gejala" element={user ? <TambahGejala /> : <Navigate to={"/masuk"}/>} />
-			<Route path="/admin/edit/gejala/:id" element={user ? <EditGejala /> : <Navigate to={"/masuk"}/>} />
-			<Route path="/admin/manage" element={user ? <Manage /> : <Navigate to={"/masuk"}/>} />
-		</Routes>
-	);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Routes>
+      <Route exact path="/" element={<Homepage />} />
+      <Route path="/penyakit" element={<DaftarPenyakit />} />
+      <Route path="/penyakit/:id" element={<Penyakit />} />
+      <Route path="/gejala" element={<DaftarGejala />} />
+      <Route path="/gejala/gejala" element={<Penyakit />} />
+      <Route path="/konsultasi" element={<PertanyaanPertama />} />
+      <Route path="/q/:id" element={<Pertanyaan />} />
+      <Route path="/q2/:id" element={<Pertanyaan />} />
+      <Route path="/hasil/:id" element={<HasilDiagnosa />} />
+      <Route path="/hasil" element={<HasilDiagnosa data={null} />} />
+      <Route path="/riwayat" element={<RiwayatKonsultasi />} />
+
+      {/* Admin Pages */}
+      <Route
+        path="/masuk"
+        element={currentUser ? <Navigate to="/" /> : <Auth />}
+      />
+      <Route
+        path="/admin"
+        element={currentUser ? <HomepageAdmin /> : <Navigate to={"/masuk"} />}
+      />
+      <Route
+        path="/admin/tambah/penyakit"
+        element={currentUser ? <TambahPenyakit /> : <Navigate to={"/masuk"} />}
+      />
+      <Route
+        path="/admin/edit/penyakit/:id"
+        element={currentUser ? <EditPenyakit /> : <Navigate to={"/masuk"} />}
+      />
+      <Route
+        path="/admin/tambah/gejala"
+        element={currentUser ? <TambahGejala /> : <Navigate to={"/masuk"} />}
+      />
+      <Route
+        path="/admin/edit/gejala/:id"
+        element={currentUser ? <EditGejala /> : <Navigate to={"/masuk"} />}
+      />
+      <Route
+        path="/admin/manage"
+        element={currentUser ? <Manage /> : <Navigate to={"/masuk"} />}
+      />
+    </Routes>
+  );
 };
 
 export default MainRouter;
--- src/pages/Pertanyaan.jsx
+++ src/pages/Pertanyaan.jsx
@@ -1,223 +1,217 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import { publicRequest } from '../AxiosInstances';
-import { consultReset, consultYes } from '../redux/consultReduces';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { publicRequest } from "../AxiosInstances";
+import { consultReset, consultYes } from "../redux/consultReduces";
 
 const Pertanyaan = () => {
-	//Storing Data
-	const [data, setData] = useState([]);
-	const { id } = useParams();
-	const [question, setQuestion] = useState(id);
-	console.log(question)
+  //Storing Data
+  const [data, setData] = useState([]);
+  const { id } = useParams();
+  const [question, setQuestion] = useState(id);
 
-	const consult = useSelector((state) => state.consult);
+  const consult = useSelector((state) => state.consult);
 
-	const location = useLocation();
-	const currentLocation = location.pathname.split('/')[1];
+  const location = useLocation();
+  const currentLocation = location.pathname.split("/")[1];
 
-	//Storing Fetching Error
-	const [err, setErr] = useState(null);
+  //Storing Fetching Error
+  const [err, setErr] = useState(null);
 
-	let navigate = useNavigate();
-	let dispatch = useDispatch();
+  let navigate = useNavigate();
+  let dispatch = useDispatch();
 
-	useEffect(() => {
-		const getData = async () => {
-			try {
-				const res = await publicRequest.get(`/symptoms/${question}`);
-				setData(res.data.data);
-				console.log(res.data);
-			} catch (err) {
-				setErr(err);
-			}
-		};
-		getData();
-	}, [question, setQuestion]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await publicRequest.get(`/symptoms/${question}`);
+        setData(res.data.data);
+        console.log(res.data);
+      } catch (err) {
+        setErr(err);
+      }
+    };
+    getData();
+  }, [question, setQuestion]);
 
-	const Answer = (event) => {
-		let answer = event.target.name;
-		let nextQuestion = '';
+  const Answer = (event) => {
+    let answer = event.target.name;
+    let nextQuestion = "";
 
-		//Jika User Klik Iya
-		if (answer === 'yes') {
-			switch (consult.nextQuestion) {
-				//Kalau Next Question Kosong, maka Pertanyaan selanjutnya leaf
-				case '':
-					dispatch(consultYes({ symptom: question.charAt(0), disease: data[0].diseaseId, nextQuestion: 'leaf' }));
-					nextQuestion = question.charAt(0);
-					setQuestion(nextQuestion);
-					break;
+    //Jika User Klik Iya
+    if (answer === "yes") {
+      switch (consult.nextQuestion) {
+        //Kalau Next Question Kosong, maka Pertanyaan selanjutnya leaf
+        case "":
+          dispatch(consultYes({ disease: id.charAt(0), nextQuestion: "leaf" }));
+          nextQuestion = question.charAt(0);
+          setQuestion(nextQuestion);
+          break;
 
-				//Kalau Next Question leaf, maka Pertanyaan selanjutnya fruit
-				case 'leaf':
-					//Kalau Gejala pada fruit kosong
-					if (data[0].fruit === null) {
-						// dispatch(consultReset())
-						navigate(`/hasil/${consult.disease}`);
+        //Kalau Next Question leaf, maka Pertanyaan selanjutnya fruit
+        case "leaf":
+          //Kalau Gejala pada fruit kosong
+          if (data[0].fruit === null) {
+            // dispatch(consultReset())
+            navigate(`/hasil/${consult.disease}`);
 
-						//Kalau Gejala pada fruit & root kosong
-					} else if (data[0].fruit === null && data[0].root === null) {
-						// dispatch(consultReset())
-						navigate(`/hasil/${consult.disease}`);
-					} else {
-						dispatch(
-							consultYes({ symptom: question.charAt(0), disease: data[0].diseaseId,  nextQuestion: 'fruit' })
-						);
-						nextQuestion = question.charAt(0);
-						setQuestion(nextQuestion);
-					}
-					break;
+            //Kalau Gejala pada fruit & root kosong
+          } else if (data[0].fruit === null && data[0].root === null) {
+            // dispatch(consultReset())
+            navigate(`/hasil/${consult.disease}`);
+          } else {
+            dispatch(
+              consultYes({ disease: id.charAt(0), nextQuestion: "fruit" })
+            );
+            nextQuestion = question.charAt(0);
+            setQuestion(nextQuestion);
+          }
 
-				//Kalau Next Question fruit, maka Pertanyaan selanjutnya root
-				case 'fruit':
-					//Kalau Gejala pada root kosong
-					if (data[0].root === null) {
-						// dispatch(consultReset())
-						navigate(`/hasil/${consult.disease}`);
-					} else {
-						dispatch(
-							consultYes({ symptom: question.charAt(0), disease: data[0].diseaseId, nextQuestion: 'root' })
-						);
-						nextQuestion = question.charAt(0);
-						setQuestion(nextQuestion);
-					}
-					break;
+          break;
 
-				//Kalau Next Question root, maka Pertanyaan konsultasi selesai dan pindah halaman riwayat
-				case 'root':
-					dispatch(consultYes({ symptom: question.charAt(0), disease: data[0].diseaseId, nextQuestion: 'end' }));
-					nextQuestion = question.charAt(0);
-					navigate(`/hasil/${consult.disease}`);
-					break;
-				default:
-					break;
-			}
+        //Kalau Next Question fruit, maka Pertanyaan selanjutnya root
+        case "fruit":
+          //Kalau Gejala pada root kosong
+          if (data[0].root === null) {
+            // dispatch(consultReset())
+            navigate(`/hasil/${consult.disease}`);
+          } else {
+            dispatch(
+              consultYes({ disease: id.charAt(0), nextQuestion: "root" })
+            );
+            nextQuestion = question.charAt(0);
+            setQuestion(nextQuestion);
+          }
+          break;
 
-			//Jika User Menjawab Tidak
-		} else if (answer === 'no') {
+        //Kalau Next Question root, maka Pertanyaan konsultasi selesai dan pindah halaman riwayat
+        case "root":
+          dispatch(consultYes({ disease: id.charAt(0), nextQuestion: "end" }));
+          nextQuestion = question.charAt(0);
+          navigate(`/hasil/${consult.disease}`);
+          break;
+        default:
+          break;
+      }
 
-			//Jika Sudah Menjawab Iya pada Pertanyaan Batang lalu menjawab tidak
-			if (consult.nextQuestion !== '') {
-				switch (consult.nextQuestion) {
+      //Jika User Menjawab Tidak
+    } else if (answer === "no") {
+      //Jika Sudah Menjawab Iya pada Pertanyaan Batang lalu menjawab tidak
+      if (consult.nextQuestion !== "") {
+        switch (consult.nextQuestion) {
+          //Kalau Next Question Kosong, maka Pertanyaan selanjutnya leaf
+          case "":
+            dispatch(
+              consultYes({ disease: id.charAt(0), nextQuestion: "leaf" })
+            );
+            nextQuestion = question.charAt(0);
+            setQuestion(nextQuestion);
+            break;
 
-					//Kalau Next Question Kosong, maka Pertanyaan selanjutnya leaf
-					case '':
-						dispatch(
-							consultYes({ symptom: question.charAt(0), disease: data[0].diseaseId, nextQuestion: 'leaf' })
-						);
-						nextQuestion = question.charAt(0);
-						setQuestion(nextQuestion);
-						break;
+          //Kalau Next Question leaf, maka Pertanyaan selanjutnya fruit
+          case "leaf":
+            //Kalau Gejala pada fruit kosong
+            if (data[0].fruit === null) {
+              // dispatch(consultReset())
+              navigate(`/hasil/${consult.disease}`);
 
-					//Kalau Next Question leaf, maka Pertanyaan selanjutnya fruit
-					case 'leaf':
+              //Kalau Gejala pada fruit & root kosong
+            } else if (data[0].fruit === null && data[0].root === null) {
+              // dispatch(consultReset())
+              navigate(`/hasil/${consult.disease}`);
+            } else {
+              dispatch(
+                consultYes({ disease: id.charAt(0), nextQuestion: "fruit" })
+              );
+              nextQuestion = question.charAt(0);
+              setQuestion(nextQuestion);
+            }
 
-						//Kalau Gejala pada fruit kosong
-						if (data[0].fruit === null) {
+            break;
 
-							// dispatch(consultReset())
-							navigate(`/hasil/${consult.disease}`);
+          //Kalau Next Question fruit, maka Pertanyaan selanjutnya root
+          case "fruit":
+            //Kalau Gejala pada root kosong
+            if (data[0].root === null) {
+              // dispatch(consultReset())
+              navigate(`/hasil/${consult.disease}`);
+            } else {
+              dispatch(
+                consultYes({ disease: id.charAt(0), nextQuestion: "root" })
+              );
+              nextQuestion = question.charAt(0);
+              setQuestion(nextQuestion);
+            }
+            break;
 
-							//Kalau Gejala pada fruit & root kosong
-						} else if (data[0].fruit === null && data[0].root === null) {
+          //Kalau Next Question root, maka Pertanyaan konsultasi selesai dan pindah halaman riwayat
+          case "root":
+            dispatch(
+              consultYes({ disease: id.charAt(0), nextQuestion: "end" })
+            );
+            nextQuestion = question.charAt(0);
+            navigate(`/hasil/${consult.disease}`);
+            break;
+          default:
+            break;
+        }
+      } else {
+        //Jika Belum menjawab iya sebelumnya
+        nextQuestion = question.substring(1);
 
-							// dispatch(consultReset())
-							navigate(`/hasil/${consult.disease}`);
-						} else {
-							dispatch(
-								consultYes({ symptom: question.charAt(0), disease: data[0].diseaseId, nextQuestion: 'fruit' })
-							);
-							nextQuestion = question.charAt(0);
-							setQuestion(nextQuestion);
-						}
+        //Jika User menjawab Tidak dan pilihan penyakit sudah habis
+        if (nextQuestion === "") {
+          navigate(`/hasil/${consult.disease}`);
+        } else {
+          //Jika User menjawab Tidak dan pilihan penyakit masih ada
+          setQuestion(nextQuestion);
+        }
+      }
+    }
+  };
 
-						break;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col gap-24 justify-center items-center">
+        {/* Questions */}
+        {data.length <= 0 ? (
+          <></>
+        ) : (
+          <h3 className="text-4xl font-bold text-indigo-900">
+            Apakah{" "}
+            {consult.nextQuestion === ""
+              ? data[0].stem
+              : consult.nextQuestion === "leaf"
+              ? data[0].leaf
+              : consult.nextQuestion === "fruit"
+              ? data[0].fruit
+              : consult.nextQuestion === "root"
+              ? data[0].root
+              : ""}{" "}
+            ?
+          </h3>
+        )}
 
-					//Kalau Next Question fruit, maka Pertanyaan selanjutnya root
-					case 'fruit':
-						//Kalau Gejala pada root kosong
-						if (data[0].root === null) {
-							// dispatch(consultReset())
-							navigate(`/hasil/${consult.disease}`);
-						} else {
-							dispatch(
-								consultYes({ symptom: question.charAt(0), disease: data[0].diseaseId, nextQuestion: 'root' })
-							);
-							nextQuestion = question.charAt(0);
-							setQuestion(nextQuestion);
-						}
-						break;
-
-					//Kalau Next Question root, maka Pertanyaan konsultasi selesai dan pindah halaman riwayat
-					case 'root':
-						dispatch(
-							consultYes({ symptom: question.charAt(0), disease: data[0].diseaseId, nextQuestion: 'end' })
-						);
-						nextQuestion = question.charAt(0);
-						navigate(`/hasil/${consult.disease}`);
-						break;
-					default:
-						break;
-				}
-			} else {
-				//Jika Belum menjawab iya sebelumnya
-				nextQuestion = question.substring(1);
-				console.log("MENOLAK!")
-
-				//Jika User menjawab Tidak dan pilihan penyakit sudah habis
-				if (nextQuestion === '') {
-					navigate(`/hasil/${consult.disease}`);
-				} else {
-					//Jika User menjawab Tidak dan pilihan penyakit masih ada
-					setQuestion(nextQuestion);
-				}
-			}
-		}
-	};
-
-	return (
-		<div className="flex justify-center items-center h-screen">
-			<div className="flex flex-col gap-24 justify-center items-center">
-				{/* Questions */}
-				{data.length <= 0 ? (
-					<></>
-				) : (
-					<h3 className="text-4xl font-bold text-indigo-900">
-						Apakah{' '}
-						{consult.nextQuestion === ''
-							? data[0].stem
-							: consult.nextQuestion === 'leaf'
-							? data[0].leaf
-							: consult.nextQuestion === 'fruit'
-							? data[0].fruit
-							: consult.nextQuestion === 'root'
-							? data[0].root
-							: ''}{' '}
-						?
-					</h3>
-				)}
-
-				{/* Answer */}
-				<div className="flex justify-center items-center gap-24">
-					<button
-						className="bg-green-500 text-white w-32 h-16 rounded-xl font-semibold hover:bg-green-600"
-						name="yes"
-						onClick={Answer}
-					>
-						Iya
-					</button>
-					<button
-						className="bg-red-500 text-white w-32 h-16 rounded-xl font-semibold hover:bg-red-600"
-						name="no"
-						onClick={Answer}
-					>
-						Tidak
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+        {/* Answer */}
+        <div className="flex justify-center items-center gap-24">
+          <button
+            className="bg-green-500 text-white w-32 h-16 rounded-xl font-semibold hover:bg-green-600"
+            name="yes"
+            onClick={Answer}
+          >
+            Iya
+          </button>
+          <button
+            className="bg-red-500 text-white w-32 h-16 rounded-xl font-semibold hover:bg-red-600"
+            name="no"
+            onClick={Answer}
+          >
+            Tidak
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Pertanyaan;
