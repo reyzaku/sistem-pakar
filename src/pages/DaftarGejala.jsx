4ed6dc7afcced93a119df652e36214ccc82a23ed
Resi Wicaksono <resiwicaksono98@gmail.com>
2022-12-06 01:38:30 +0700

    add select option   in page TambahGejala

--- src/pages/DaftarGejala.jsx
+++ src/pages/DaftarGejala.jsx
@@ -1,73 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { publicRequest } from '../AxiosInstances';
+import React, { useEffect, useState } from "react";
+import { publicRequest } from "../AxiosInstances";
 
 const DaftarGejala = () => {
-	//Storing Data Penyakit
-	const [gejala, setGejala] = useState([]);
+  //Storing Data Penyakit
+  const [gejala, setGejala] = useState([]);
 
-	//Storing Fetching Error
-	const [err, setErr] = useState(null);
+  //Storing Fetching Error
+  const [err, setErr] = useState(null);
 
-	useEffect(() => {
-		const getData = async () => {
-			try {
-				const res = await publicRequest.get(`/symptoms`);
-				setGejala(res.data.data);
-			} catch (err) {
-				setErr('Err');
-			}
-		};
-		getData();
-	}, []);
-	return (
-		<div>
-			<div className="overflow-x-auto relative mt-16">
-				<h3 className="font-bold text-3xl text-indigo-900 mb-8">
-					Daftar Gejala
-				</h3>
-				<table className="w-full text-sm text-left text-gray-500">
-					<thead className="text-md text-gray-50 uppercase bg-gray-700">
-						<tr>
-							<th scope="col" className="py-3 px-6">
-								Kode Penyakit
-							</th>
-							<th scope="col" className="py-3 px-6">
-								Batang
-							</th>
-							<th scope="col" className="py-3 px-6">
-								Daun
-							</th>
-							<th scope="col" className="py-3 px-6">
-								Buah
-							</th>
-							<th scope="col" className="py-3 px-6">
-								Akar
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						{gejala?.map((item, index) => (
-							<tr className="bg-white border-b">
-								<td className="py-4 px-6">P000{item.diseaseId}</td>
-								<td className="py-4 px-6">
-									{item.stem !== null ? item.stem : '-'}
-								</td>
-								<td className="py-4 px-6">
-									{item.leaf !== null ? item.leaf : '-'}
-								</td>
-								<td className="py-4 px-6">
-									{item.fruit !== null ? item.fruit : '-'}
-								</td>
-								<td className="py-4 px-6">
-									{item.root !== null ? item.root : '-'}
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    const getAllDisease = async () => {
+      try {
+        const res = await publicRequest.get(`/symptoms`);
+        setGejala(res.data.data);
+      } catch (err) {
+        setErr("Err");
+      }
+    };
+    getAllDisease();
+  }, []);
+  return (
+    <div>
+      <div className="overflow-x-auto relative mt-16">
+        <h3 className="font-bold text-3xl text-indigo-900 mb-8">
+          Daftar Gejala
+        </h3>
+        <table className="w-full text-sm text-left text-gray-500">
+          <thead className="text-md text-gray-50 uppercase bg-gray-700">
+            <tr>
+              <th scope="col" className="py-3 px-6">
+                Kode Penyakit
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Batang
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Daun
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Buah
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Akar
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {gejala?.map((item, index) => (
+              <tr className="bg-white border-b">
+                <td className="py-4 px-6">P000{item.diseaseId}</td>
+                <td className="py-4 px-6">
+                  {item.stem !== null ? item.stem : "-"}
+                </td>
+                <td className="py-4 px-6">
+                  {item.leaf !== null ? item.leaf : "-"}
+                </td>
+                <td className="py-4 px-6">
+                  {item.fruit !== null ? item.fruit : "-"}
+                </td>
+                <td className="py-4 px-6">
+                  {item.root !== null ? item.root : "-"}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default DaftarGejala;
--- src/pages/admin/TambahGejala.jsx
+++ src/pages/admin/TambahGejala.jsx
@@ -1,145 +1,157 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { publicRequest } from '../../AxiosInstances';
-import PopupAlert from '../../component/PopupAlert';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { authRequest, publicRequest } from "../../AxiosInstances";
+import PopupAlert from "../../component/PopupAlert";
 
 const TambahGejala = () => {
-	//stored input Value
-	const [gejala, setGejala] = useState({});
+  //stored input Value
+  const [gejala, setGejala] = useState({});
+  const [disease, setDisease] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [err, setErr] = useState(null);
+  let navigate = useNavigate();
 
-	const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const getAllDisease = async () => {
+      const { data } = await authRequest.get("/diseases");
+      setDisease(data.data);
+    };
+    getAllDisease();
+  }, []);
 
-	const [err, setErr] = useState(null);
+  //Send Value Handler
+  const submitHandle = (e) => {
+    e.preventDefault();
+    setIsOpen(false);
+    publicRequest
+      .post("/symptoms", gejala)
+      .then(() => {
+        navigate("/admin/manage");
+      })
+      .catch((error) => {
+        setErr(error);
+        console.log(error);
+        setIsOpen(true);
+      });
+  };
 
-	let navigate = useNavigate();
+  return (
+    <>
+      {/* <PopupAlert isOpen={openModal}/> */}
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <h2 className="mb-16 font-bold text-3xl text-indigo-900">
+          Tambah Data Gejala
+        </h2>
+        {isOpen && (
+          <h4 className="text-red-500 font-thin mb-16 text-center">
+            {err.message}
+            <br />
+            {err.name}
+          </h4>
+        )}
+        <div className="w-full bg-white rounded-l md:mt-0 sm:max-w-md xl:p-0 ">
+          <form action="submit" className="flex flex-col gap-8">
+            <div className="flex flex-col gap-2">
+              <label
+                for="disease"
+                class="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Penyakit
+              </label>
+              <select
+                name="disease"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                onChange={(e) =>
+                  setGejala({ ...gejala, diseaseId: e.target.value })
+                }
+              >
+                <option value="disable">Pilih Penyakit</option>
+                {disease.map((data, i) => (
+                  <option value={data.id} key={i}>
+                    {data.name}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-	//Send Value Handler
-	const submitHandle = (e) => {
-		e.preventDefault();
-		setIsOpen(false)
-		publicRequest
-			.post('/symptoms', gejala)
-			.then(() => {
-				navigate('/admin/manage');
-			})
-			.catch((error) => {
-				setErr(error);
-				console.log(error);
-				setIsOpen(true);
-			});
-	};
+            <div className="flex flex-col gap-2">
+              <label
+                for="stem"
+                class="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Gejala pada Batang
+              </label>
+              <input
+                type="text"
+                name="stem"
+                placeholder=""
+                onChange={(e) => setGejala({ ...gejala, stem: e.target.value })}
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+              />
+            </div>
 
-	return (
-		<>
-			{/* <PopupAlert isOpen={openModal}/> */}
-			<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-				<h2 className="mb-16 font-bold text-3xl text-indigo-900">
-					Tambah Data Gejala
-				</h2>
-				{isOpen && (
-					<h4 className="text-red-500 font-thin mb-16 text-center">
-						{err.message}<br/>
-						{err.name}
-					</h4>
-				)}
-				<div className="w-full bg-white rounded-l md:mt-0 sm:max-w-md xl:p-0 ">
-					<form action="submit" className="flex flex-col gap-8">
-						<div className="flex flex-col gap-2">
-							<label
-								for="disease"
-								class="block mb-2 text-sm font-medium text-gray-900"
-							>
-								Kode Penyakit
-							</label>
-							<input
-								type="text"
-								name="disease"
-								placeholder="kode_penyakit"
-								onChange={(e) =>
-									setGejala({ ...gejala, diseaseId: e.target.value })
-								}
-								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-							/>
-						</div>
+            <div className="flex flex-col gap-2">
+              <label
+                for="leaf"
+                class="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Gejala pada Daun
+              </label>
+              <input
+                type="text"
+                name="leaf"
+                placeholder=""
+                onChange={(e) => setGejala({ ...gejala, leaf: e.target.value })}
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+              />
+            </div>
 
-						<div className="flex flex-col gap-2">
-							<label
-								for="stem"
-								class="block mb-2 text-sm font-medium text-gray-900"
-							>
-								Gejala pada Batang
-							</label>
-							<input
-								type="text"
-								name="stem"
-								placeholder=""
-								onChange={(e) => setGejala({ ...gejala, stem: e.target.value })}
-								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-							/>
-						</div>
+            <div className="flex flex-col gap-2">
+              <label
+                for="fruit"
+                class="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Gejala pada Buah
+              </label>
+              <input
+                type="text"
+                name="fruit"
+                placeholder=""
+                onChange={(e) =>
+                  setGejala({ ...gejala, fruit: e.target.value })
+                }
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+              />
+            </div>
 
-						<div className="flex flex-col gap-2">
-							<label
-								for="leaf"
-								class="block mb-2 text-sm font-medium text-gray-900"
-							>
-								Gejala pada Daun
-							</label>
-							<input
-								type="text"
-								name="leaf"
-								placeholder=""
-								onChange={(e) => setGejala({ ...gejala, leaf: e.target.value })}
-								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-							/>
-						</div>
+            <div className="flex flex-col gap-2">
+              <label
+                for="root"
+                class="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Gejala pada Akar
+              </label>
+              <input
+                type="text"
+                name="root"
+                placeholder=""
+                onChange={(e) => setGejala({ ...gejala, root: e.target.value })}
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+              />
+            </div>
 
-						<div className="flex flex-col gap-2">
-							<label
-								for="fruit"
-								class="block mb-2 text-sm font-medium text-gray-900"
-							>
-								Gejala pada Buah
-							</label>
-							<input
-								type="text"
-								name="fruit"
-								placeholder=""
-								onChange={(e) =>
-									setGejala({ ...gejala, fruit: e.target.value })
-								}
-								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-							/>
-						</div>
-
-						<div className="flex flex-col gap-2">
-							<label
-								for="root"
-								class="block mb-2 text-sm font-medium text-gray-900"
-							>
-								Gejala pada Akar
-							</label>
-							<input
-								type="text"
-								name="root"
-								placeholder=""
-								onChange={(e) => setGejala({ ...gejala, root: e.target.value })}
-								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-							/>
-						</div>
-
-						<button
-							type="submit"
-							onClick={submitHandle}
-							class="w-full text-white bg-indigo-900 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-						>
-							Tambah Gejala
-						</button>
-					</form>
-				</div>
-			</div>
-		</>
-	);
+            <button
+              type="submit"
+              onClick={submitHandle}
+              class="w-full text-white bg-indigo-900 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Tambah Gejala
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TambahGejala;
